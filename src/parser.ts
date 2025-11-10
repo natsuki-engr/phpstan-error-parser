1146d08bb7c2cfae85107e6489499e864813c145
natsuki ueda <biafrajr@gmail.com>
2025-11-10 21:20:51 +0900

    add line_breaks property to token definitions for consistency

--- src/parser.ts
+++ src/parser.ts
@@ -1,16 +1,16 @@
 import { createToken, CstParser, Lexer } from "chevrotain";
 
 const tokens = {
-  FUNCTION_NAME: createToken({ name: "FunctionName", pattern: /(?<!(A|a)nonymous function )(?<=(F|f)unction )[a-zA-Z0-9_]+(\\[a-zA-Z0-9_]+)*/ }),
-  METHOD_NAME: createToken({ name: "MethodName", pattern: /(?<=(M|m)ethod )[a-zA-Z0-9_]+(\\[a-zA-Z0-9_]+)*/ }),
-  COMMON_WORD: createToken({ name: "CommonWord", pattern: /[a-zA-Z]+/ }),
-  SPACE: createToken({ name: "space", pattern: /\s+/, group: Lexer.SKIPPED }),
-  PERIOD: createToken({ name: "period", pattern: "." }),
-  DOC_TAG: createToken({ name: "DocTag", pattern: /@[\w](-\w)*/ }),
-  VARIABLE: createToken({ name: "Variable", pattern: /\$[a-z][a-zA-Z0-9_]*/ }),
+  FUNCTION_NAME: createToken({ name: "FunctionName", pattern: /(?<!(A|a)nonymous function )(?<=(F|f)unction )[a-zA-Z0-9_]+(\\[a-zA-Z0-9_]+)*/, line_breaks: false }),
+  METHOD_NAME: createToken({ name: "MethodName", pattern: /(?<=(M|m)ethod )[a-zA-Z0-9_]+(\\[a-zA-Z0-9_]+)*/, line_breaks: false }),
+  COMMON_WORD: createToken({ name: "CommonWord", pattern: /[a-zA-Z]+/, line_breaks: false }),
+  SPACE: createToken({ name: "space", pattern: /\s+/, group: Lexer.SKIPPED, line_breaks: false }),
+  PERIOD: createToken({ name: "period", pattern: ".", line_breaks: false }),
+  DOC_TAG: createToken({ name: "DocTag", pattern: /@[\w](-\w)*/, line_breaks: false }),
+  VARIABLE: createToken({ name: "Variable", pattern: /\$[a-z][a-zA-Z0-9_]*/, line_breaks: false }),
 };
 
-export const lexer = new Lexer(Object.values(tokens));
+export const lexer = new Lexer(Object.values(tokens), {positionTracking: "onlyOffset"});
 
 export class Parser extends CstParser {
   constructor() {
